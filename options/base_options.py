e9acc82cb33dcffded7673fcd2c87ff14dfd64e1
gkalstn000 <gkalstn000@gmail.com>
2023-05-09 10:26:44 +0900

    lr 0.0001 -> 0.0002 L1loss 제거 norm_G spectralsyncbatch -> spectralinsatance decoder nord_D -> norm_G

--- options/base_options.py
+++ options/base_options.py
@@ -22,7 +22,7 @@ class BaseOptions():
         parser.add_argument('--id', type=str, default='dptn', help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--model', type=str, default='dptn',help='name of the model.')
         parser.add_argument('--norm_E', type=str, default='spectralinstance', help='[spectralinstance|spectralbatch|spectralsyncbatch]')
-        parser.add_argument('--norm_G', type=str, default='spectralinstance', help='[spectralinstance|spectralbatch|spectralsyncbatch]')
+        parser.add_argument('--norm_G', type=str, default='spectralsyncbatch', help='[spectralinstance|spectralbatch|spectralsyncbatch]')
         parser.add_argument('--norm_D', type=str, default='spectralinstance', help='[spectralinstance|spectralbatch|spectralsyncbatch]')
         parser.add_argument('--gpu_ids', type=str, default='1,2,3', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
